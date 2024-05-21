aae42f6dd21d85a2cb3c29c689b037e631fd6b06
Adryan Albuquerque <81368367+Adryan-raf@users.noreply.github.com>
2024-05-21 17:09:34 -0300

    Create farm.py

---
+++ Challenges/Farm Challenge/farm.py
@@ -0,0 +1,18 @@
+from AnimalsClass import *
+from CatClass import *
+from DogClass import *
+from HabbitClass import *
+from CowClass import *
+
+cat1= Cat("Happy","blue")
+dog1= Dog("Scooby", "German Dog")
+habbit1=Habbit("Buggs Bunny", "hare")
+cow1=Cow("Ben", "normal")
+
+cat1.eat()
+dog1.bark()
+cat1.meow()
+dog1.eat()
+habbit1.sizzle()
+cow1.moo()
+cow1.eat()
